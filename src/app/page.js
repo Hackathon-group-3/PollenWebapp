8618c75633e0aa119b29c4cbf74a81c18aec704b
Alejandro Patino Camargo <Alejandropatinoc@gmail.com>
2025-05-04 00:58:35 -0700

    render Map in root

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Map from "./components/Map/Map.js";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Map />
       <main className={styles.main}>
         <Image
           className={styles.logo}
