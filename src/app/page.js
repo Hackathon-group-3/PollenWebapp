d8ee640532bd414a268ccc148b3d4008c6ef6afb
Alejandro Patino Camargo <Alejandropatinoc@gmail.com>
2025-05-04 12:50:45 -0700

    page fetches API and passes data through props

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,56 @@
 "use client";
-
-import dotenv from "dotenv";
-
 import Location_component from "./components/locationSearch/locationSearch";
 import styles from "./page.module.css";
 import Map from "./components/Map/Map";
-
-
-
-dotenv.config();
-
+import { useState } from "react";
 
 export default function Home() {
+  const [forecastData, setForecastData] = useState(null);
+  const [geoData, setGeoData] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleSearch(zipcode) {
+    try {
+      setError(null);
+
+      const geoResponse = await fetch(`/api/v1/geodata?zipcode=${zipcode}`);
+
+      if (!geoResponse.ok) {
+        const err = await geoResponse.json();
+        throw new Error(err.message || "Failed to fetch geolocation");
+      }
+
+      const { data } = await geoResponse.json();
+      setGeoData(data);
+
+      const forecastResponse = await fetch(
+        `/api/v1/forecast?latitude=${data.latitude}&longitude=${data.longitude}`,
+      );
+
+      if (!forecastResponse.ok) {
+        const err = await forecastResponse.json();
+        throw new Error(err.message || "Failed to fetch forecast");
+      }
+
+      const forecast = (await forecastResponse.json()).data;
+
+      setForecastData(forecast);
+    } catch (error) {
+      setError(error.message);
+      setForecastData(null);
+      setGeoData(null);
+    }
+  }
+
   return (
     <div className={styles.page}>
-
       <main className={styles.main}>
-        <Location_component/>
-        <Map/>
+        <Location_component onSearch={handleSearch} />
 
-        
-
+        {geoData && <Map geoData={geoData} forecastData={forecastData} />}
       </main>
-      <footer className={styles.footer}>
 
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
-
-
-
