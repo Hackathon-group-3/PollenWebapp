1a1bdb72d6cb677550f3511e22cbda81b534d27c
Alejandro Patino Camargo <Alejandropatinoc@gmail.com>
2025-05-04 12:48:21 -0700

    refactored MAP component to fetch static image

--- src/app/components/Map/Map.js
+++ src/app/components/Map/Map.js
@@ -1,24 +1,20 @@
-import { getGeoLocation } from "@/app/lib/getGeoLocation";
-import styles from "./map.module.css";
-import Image from "next/image";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "./map.module.css";
 
 export default function Map({ geoData }) {
   const [locationData, setLocationData] = useState(geoData);
 
   useEffect(() => {
-    if (!geoData) {
-      getGeoLocation().then((data) => {
-        setLocationData(data);
-      });
+    if (geoData) {
+      setLocationData(geoData);
     }
   }, [geoData]);
 
   if (!locationData) return <p>Unable to load map data.</p>;
 
   const { latitude, longitude, cityName } = locationData;
-  const apiKey = process.env.API_KEY;
-  const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=400x400&key=${apiKey}`;
+  const mapUrl = `/api/v1/staticmap?latitude=${latitude}&longitude=${longitude}`;
 
   return (
     <div className={styles.container}>
@@ -26,19 +22,19 @@ export default function Map({ geoData }) {
         <div className={styles.text}>
           <h1>Location</h1>
           <p>
-            Showing data for
-            <span className={styles.city}> {cityName}</span>
+            Showing data for <span className={styles.city}>{cityName}</span>
           </p>
         </div>
-          <Image
-            className={styles.map}
-            src={mapUrl}
-            alt="Static Map"
-            width={400}
-            height={400}
-            priority
-          />
-        </div>
+        <Image
+          className={styles.map}
+          src={mapUrl}
+          alt={`Static map of ${cityName}`}
+          width={400}
+          height={400}
+          loading="lazy"
+        />
       </div>
+    </div>
   );
 }
+
