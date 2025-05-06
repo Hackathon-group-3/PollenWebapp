3f4d816837991f17bde14220a0f7df0845aa0def
“Donovan <doctorwaffles2000@gmail.com>
2025-05-06 01:47:14 -0400

    Update locationSearch.js

--- src/app/components/locationSearch/locationSearch.js
+++ src/app/components/locationSearch/locationSearch.js
@@ -34,7 +34,7 @@ export default function Location_component( { onSearch }) {
                     className = {styles.location_input} 
                     id = {styles.location_text_input}
                     type="text"
-                    placeholder="Enter a zip code"
+                    placeholder="Enter a zip code or address"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                     />
